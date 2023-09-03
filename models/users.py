360ba03f93467772c2b6c758a3890a2945d60cfc
wooseong <ddk74100@naver.com>
2023-09-03 23:46:04 +0900

    Create: 5장 수

---
+++ models/users.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+from pydantic import BaseModel, EmailStr
+from models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    username: str
+    events: Optional[List[Event]] = None
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+            }
+        }
